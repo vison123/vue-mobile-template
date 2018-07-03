968b1c3892b8ca7d3e8b0bc24ae962397205a51f
韦森 <723955399@qq.com>
2018-07-03 10:22:13 +0800

    feat: update build webpack ChunkName

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import { routerMode } from '../framework/env'
 
-const Home = () => import('@/pages/home/home')
-const Search = () => import('@/pages/search/search')
-const Order = () => import('@/pages/order/order')
-const Profile = () => import('@/pages/profile/profile')
+const Home = () => import(/* webpackChunkName: "home" */'@/pages/home/home')
+const Search = () => import(/* webpackChunkName: "search" */'@/pages/search/search')
+const Order = () => import(/* webpackChunkName: "order" */'@/pages/order/order')
+const Profile = () => import(/* webpackChunkName: "profile" */'@/pages/profile/profile')
 
 Vue.use(Router)
 
